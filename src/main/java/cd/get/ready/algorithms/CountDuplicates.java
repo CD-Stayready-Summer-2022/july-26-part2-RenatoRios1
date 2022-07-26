9461aa7d23836a617ddbe80ec0a9d142d3125674
RenatoRios1 <renatoarios1@gmail.com>
2022-07-26 09:39:28 -0500

    Done

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,8 @@
 package cd.get.ready.algorithms;
 
+import java.beans.IntrospectionException;
+import java.util.HashMap;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +10,16 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        Integer count = 0;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for(String each: input ){
+            if(!map.containsKey(each)){
+                map.put(each, 1);
+            }else{
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -16,6 +28,18 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        Integer count = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(Integer each: input ){
+            if(!map.containsKey(each)){
+                map.put(each, 1);
+            }else{
+                map.put(each, map.get(each)+1);
+                if(!(map.get(each)>2)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
